aad892fb5e788acf3df3fad8b67c49a92b7dc237
tindleaj <tindleaj@gmail.com>
2017-01-24 09:37:21 -0500

    made npm project and added gulp

--- php/header.php
+++ php/header.php
@@ -8,8 +8,15 @@
     <!-- Meta tag title -->
     <title><?php echo $Site->title() ?></title>
 
+
+    <?php /*
+           * These just load static assets, the bludit has a Theme object with methods that default
+           * to looking for the assets in different folders than what we have set up here,
+           * so the paths are declared explicitly using the HTML_PATH_THEME constant. 
+           */ ?>
+
     <!-- CSS -->
-    <?php Theme::css('styles.css', HTML_PATH_THEME . 'public/css/'); ?>
+    <?php Theme::css('main.css', HTML_PATH_THEME . 'public/css/'); ?>
 
     <!-- JavaScript -->
     <?php Theme::javascript('main.js', HTML_PATH_THEME . 'public/js/'); ?>
